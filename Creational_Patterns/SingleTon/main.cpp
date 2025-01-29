a33194871e9a6f2bbcf75736a579a3e091e8cefe
gucci1909 <umangarora0134@gmail.com>
2025-01-29 16:44:27 +0530

    creational-patterns done

---
+++ Creational_Patterns/SingleTon/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class Database {
+private:
+    static Database* instance;
+
+    Database() {}
+public:
+    static Database* getInstance() {
+        if (!instance) {
+            instance = new Database();
+        }
+        return instance;
+    }
+
+    void connect() {
+        cout << "Connected to Database!" << endl;
+    }
+};
+
+Database* Database::instance = nullptr;
+
+int main() {
+    Database* db = Database::getInstance();
+    db->connect();
+    return 0;
+}
